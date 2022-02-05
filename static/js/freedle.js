38053dc4e2cc6e29963ac4c575a12c83d8afee74
Aadi Kuchlous <aadikuchlous@gmail.com>
2022-02-05 23:20:48 +0530

    google analytics gtag test

--- static/js/freedle.js
+++ static/js/freedle.js
@@ -485,6 +485,7 @@ function addGameStats(guess_no) {
   else {
     stats.gamesWon += 1;
     stats.guesses[guess_no.toString()] += 1;
+    gtag('event', 'GameWon');
   }
 
   // Extra '*10' for one decimal place rounding
